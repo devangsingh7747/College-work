ae177367c35ef69eafa9d9f167c4f76e5a4ace95
Devang Singh <devangsinghchauhan933@gmail.com>
2025-03-09 11:22:11 +0530

    updated new problem

--- aaps1.java
+++ aaps1.java
@@ -69,15 +69,32 @@
 
 
 
+// public class aaps1 {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 10; i++) {
+            
+//             if (i == 3 || i == 6) {
+//                 continue;
+//             }
+
+//             System.out.print(i + " ");
+//         }
+//     }
+// }
+
+
+
+
 public class aaps1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            
-            if (i == 3 || i == 6) {
-                continue;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                
+                System.out.println("i = " + i + ", j = " + j);
             }
-
-            System.out.print(i + " ");
         }
     }
 }
