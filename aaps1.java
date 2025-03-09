a5852bf2dd6a1b5ed2c7582efaa7c5fcbba428ad
Devang Singh <devangsinghchauhan933@gmail.com>
2025-03-09 10:41:53 +0530

    updated program

---
+++ aaps1.java
@@ -0,0 +1,29 @@
+// class aaps1 {
+//     public static void main(String[] args) {
+//         int start=10, end=30;
+//         for(int i=start; i<=end; i++) {
+//             if(isPrime(i)) {
+//                 System.out.print(i + " ");
+//             }
+//         }
+//     }
+//     static boolean isPrime(int n) {
+//         if(n<2) return false;
+//         for(int i=2; i*i<=n; i++) {
+//             if(n%i == 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+public class aaps1 {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x++ <= 7) {
+            
+        }
+        System.out.println(x);
+    }
+}
